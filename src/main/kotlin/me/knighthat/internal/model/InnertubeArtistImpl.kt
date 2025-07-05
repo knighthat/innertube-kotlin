51905980fece14620b60e12c7821253e742166df
knighthat <git@knighthat.me>
2025-07-05 16:57:04 -0500

    implement parsers & port tests of InnertubeArtistImpl

--- src/main/kotlin/me/knighthat/internal/model/InnertubeArtistImpl.kt
+++ src/main/kotlin/me/knighthat/internal/model/InnertubeArtistImpl.kt
@@ -1,8 +1,14 @@
 package me.knighthat.internal.model
 
 import kotlinx.serialization.Serializable
+import me.knighthat.innertube.PageType
 import me.knighthat.innertube.model.InnertubeArtist
 import me.knighthat.innertube.model.InnertubeItem
+import me.knighthat.innertube.model.InnertubeSong
+import me.knighthat.innertube.response.BrowseResponse
+import me.knighthat.innertube.response.MusicCarouselShelfRenderer
+import me.knighthat.innertube.response.MusicShelfRenderer
+import me.knighthat.innertube.response.MusicTwoRowItemRenderer
 import me.knighthat.innertube.response.Thumbnails
 
 @Serializable
@@ -16,6 +22,125 @@ internal data class InnertubeArtistImpl(
     override val shortNumMonthlyAudience: String?,
     override val sections: List<InnertubeArtist.Section>
 ): InnertubeArtist {
+
+    companion object {
+
+        private fun parse( renderer: MusicCarouselShelfRenderer ): InnertubeArtist.Section? {
+            // Skip this section because 1 or all content isn't [MusicTwoRowItemRender]
+            if( renderer.contents.any { it.musicTwoRowItemRenderer == null } )
+                return null
+
+            val sectionHeader = renderer.header.musicCarouselShelfBasicHeaderRenderer
+            val run = sectionHeader.title.runs.firstOrNull()
+            val browse = run?.navigationEndpoint?.browseEndpoint
+
+            return SectionImpl(
+                sectionHeader.title.firstText,
+                browse?.browseId,
+                browse?.params,
+                renderer.contents
+                        .mapNotNull( MusicCarouselShelfRenderer.Content::musicTwoRowItemRenderer )
+                        .mapNotNull { itemRenderer ->
+                            var result: InnertubeItem? = null
+
+                            itemRenderer.navigationEndpoint
+                                        .browseEndpoint
+                                        ?.browseEndpointContextSupportedConfigs
+                                        ?.browseEndpointContextMusicConfig
+                                        ?.pageType
+                                        ?.also { pageType ->
+                                            result = when( pageType ) {
+                                                PageType.ARTIST     -> from( itemRenderer )
+                                                PageType.ALBUM      -> InnertubeAlbumImpl.from( itemRenderer )
+                                                PageType.PLAYLIST   -> InnertubePlaylistImpl.from( itemRenderer )
+                                                else                -> null
+                                            }
+                                        }
+
+                            itemRenderer.navigationEndpoint
+                                        .watchEndpoint
+                                        ?.also {
+                                            result = InnertubeSongImpl.from( itemRenderer )
+                                        }
+
+                            result
+                        }
+            )
+        }
+
+        fun from( renderer: MusicTwoRowItemRenderer ): InnertubeArtist {
+            val run = renderer.title.runs.first()       // Requires not null to proceed
+
+            return InnertubeArtistImpl(
+                // [id] must be a non-null value
+                run.navigationEndpoint!!.browseEndpoint!!.browseId,
+                run.text,
+                renderer.thumbnailRenderer.toThumbnailList(),
+                null,
+                null,
+                null,
+                renderer.subtitle.firstText,
+                emptyList()
+            )
+        }
+
+        fun from( response: BrowseResponse ): InnertubeArtist {
+            val contents = requireNotNull(
+                response.contents
+                        .singleColumnBrowseResultsRenderer
+                        ?.tabs
+                        ?.firstOrNull()
+                        ?.tabRenderer
+                        ?.content
+                        ?.sectionListRenderer
+                        ?.contents
+            )
+
+            val sections = ArrayList<InnertubeArtist.Section>(8)
+            var description: String? = null
+            for( content in contents ) {
+                content.musicShelfRenderer?.also { musicSection ->
+                    val browse = musicSection.bottomEndpoint?.browseEndpoint
+                    val songs: List<InnertubeSong> = musicSection.contents
+                                                                 .map( MusicShelfRenderer.Content::musicResponsiveListItemRenderer )
+                                                                 .map( InnertubeSongImpl::from )
+
+                    sections.add(
+                        SectionImpl(musicSection.title.firstText, browse?.browseId, browse?.params, songs)
+                    )
+                }
+
+                content.musicDescriptionShelfRenderer
+                       ?.description
+                       ?.firstText
+                       ?.also { description = it }
+
+                // This section contains Albums, Single & EPs, related Artists, and Playlists.
+                content.musicCarouselShelfRenderer
+                       ?.let( ::parse )
+                       ?.also( sections::add )
+            }
+
+            val header = requireNotNull(
+                response.header?.musicImmersiveHeaderRenderer
+            )
+            val subscribeButton = requireNotNull(
+                header.subscriptionButton.subscribeButtonRenderer
+            )
+
+            return InnertubeArtistImpl(
+                subscribeButton.channelId,
+                header.title.firstText,
+                header.thumbnail.toThumbnailList(),
+                description,
+                subscribeButton.shortSubscriberCountText.firstText,
+                subscribeButton.longSubscriberCountText.firstText,
+                header.monthlyListenerCount.firstText,
+                sections
+            )
+        }
+    }
+
     @Serializable
     internal data class SectionImpl(
         override val title: String,
---
+++ src/test/kotlin/me/knighthat/internal/model/InnertubeArtistImplTest.kt
@@ -0,0 +1,81 @@
+package me.knighthat.internal.model
+
+import me.knighthat.innertube.decode
+import me.knighthat.innertube.response.MusicTwoRowItemRenderer
+import me.knighthat.internal.response.BrowseResponseImpl
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertNotNull
+import org.junit.jupiter.api.assertNull
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+
+class InnertubeArtistImplTest {
+
+    @Test
+    fun testFromMusicTwoRowItemRenderer() {
+        val fileName = "ytm/browse/artist_related_artists_musicTwoRowItemRenderer.json"
+        ClassLoader.getSystemResourceAsStream( fileName )
+                   .also( ::assertNotNull )
+                   ?.use { inStream ->
+                       val renderer = inStream.decode<MusicTwoRowItemRenderer>()
+                       assertNotNull( renderer )
+
+                       val artist = InnertubeArtistImpl.from( renderer )
+                       assertEquals( "UCRY5dYsbIN5TylSbd7gVnZg", artist.id )
+                       assertEquals( "Kanye West", artist.name )
+                       assertEquals( 2, artist.thumbnails.size )
+                       assertNull( artist.description )
+                       assertNull( artist.longNumSubscribers )
+                       assertNull( artist.shortNumSubscribers )
+                       assertNotNull( artist.shortNumMonthlyAudience )
+                       assertEquals( "144M monthly audience", artist.shortNumMonthlyAudience )
+                       assertTrue( artist.sections.isEmpty() )
+                   }
+    }
+
+    @Test
+    fun testFromBrowseResponse() {
+        val fileName = "ytm/browse/artist_browseResponse.json"
+        ClassLoader.getSystemResourceAsStream( fileName )
+                   .also( ::assertNotNull )
+                   ?.use { inStream ->
+                       val renderer = inStream.decode<BrowseResponseImpl>()
+                       assertNotNull( renderer )
+
+                       val artist = InnertubeArtistImpl.from( renderer )
+                       assertEquals( "UC3lBXcrKFnFAFkfVk5WuKcQ", artist.id)
+                       assertEquals( "Kendrick Lamar", artist.name)
+
+                       assertEquals( 5, artist.thumbnails.size )
+
+                       assertNotNull( artist.description )
+                       // Due to multiple new lines, triple quotes must be used to preserve all of them
+                       val expectedDescription = """
+                           Kendrick Lamar Duckworth is an American rapper, songwriter and record producer. Regarded as one of the greatest rappers of all time, he was awarded the 2018 Pulitzer Prize for Music, becoming the first musician outside of the classical and jazz genres to receive the award.
+                           Born in Compton, California, Lamar began releasing music under the stage name K.Dot while attending high school. He signed with Top Dawg Entertainment in 2005 and co-founded the hip hop supergroup Black Hippy there. Following the 2011 release of his alternative rap debut album Section.80, Lamar secured a joint contract with Dr. Dre's Aftermath Entertainment and Interscope Records. He rose to prominence with his gangsta rap-influenced second album Good Kid, M.A.A.D City, which became the longest-charting hip hop studio album in Billboard 200 history; Rolling Stone named it the greatest concept album of all time.
+    
+                           From Wikipedia (https://en.wikipedia.org/wiki/Kendrick_Lamar) under Creative Commons Attribution CC-BY-SA 3.0 (http://creativecommons.org/licenses/by-sa/3.0/legalcode)
+                       """.trimIndent()
+                       assertEquals( expectedDescription, artist.description!! )
+
+                       assertNotNull( artist.longNumSubscribers )
+                       assertEquals( "19.1M subscribers", artist.longNumSubscribers )
+
+                       assertNotNull( artist.shortNumSubscribers )
+                       assertEquals( "19.1M", artist.shortNumSubscribers )
+
+                       assertNotNull( artist.shortNumMonthlyAudience )
+                       assertEquals( "434M monthly audience", artist.shortNumMonthlyAudience )
+
+                       artist.sections.forEach {
+                           println(it.title)
+                       }
+                       assertEquals(
+                           7,      // musicDescriptionShelfRenderer is excluded
+                           artist.sections
+                                 .filter { it.contents.isNotEmpty() }
+                                 .size
+                       )
+                   }
+    }
+}
