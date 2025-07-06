5da33875cfb9314fff82c580d5780bcaa47399b7
knighthat <git@knighthat.me>
2025-07-06 18:13:26 -0500

    simple request template to Innertube and tests

---
+++ src/main/kotlin/me/knighthat/innertube/Innertube.kt
@@ -0,0 +1,118 @@
+package me.knighthat.innertube
+
+import kotlinx.serialization.json.Json
+import me.knighthat.innertube.model.ContinuedPlaylist
+import me.knighthat.innertube.model.InnertubePlaylist
+import me.knighthat.innertube.request.Localization
+import me.knighthat.innertube.request.Request
+import me.knighthat.innertube.request.body.BrowseBody
+import me.knighthat.innertube.request.body.Context
+import me.knighthat.innertube.request.body.RequestBody
+import me.knighthat.innertube.response.Response
+import me.knighthat.internal.model.ContinuedPlaylistImpl
+import me.knighthat.internal.model.InnertubePlaylistImpl
+import me.knighthat.internal.response.BrowseResponseImpl
+import org.intellij.lang.annotations.MagicConstant
+import org.jetbrains.annotations.Blocking
+import org.jetbrains.annotations.VisibleForTesting
+import java.io.IOException
+
+
+object Innertube {
+
+    private val JSON: Json = Json {
+        ignoreUnknownKeys = true
+        explicitNulls = false
+    }
+
+    var client: Provider? = null
+
+    @VisibleForTesting
+    @Blocking
+    @Throws(IOException::class)
+    fun sendRequest(
+        @MagicConstant(valuesFromClass = Request::class) method: String,
+        host: String,
+        @MagicConstant(valuesFromClass = Endpoints::class) endpoint: String,
+        requestBody: RequestBody,
+        headers: Map<String, List<String>>
+    ): Response = client!!.execute(
+        Request(method, headers, "$host/$endpoint", requestBody)
+    )
+
+
+    // END: Static fields/functions
+    @Throws(IOException::class)
+    private fun ytmBrowse( browseBody: BrowseBody, headers: Map<String, List<String>> ): Response =
+        sendRequest(Request.POST, Constants.YOUTUBE_MUSIC_URL, Endpoints.BROWSE, browseBody, headers )
+
+    fun browsePlaylist(
+        playlistId: String,
+        localization: Localization
+    ): Result<InnertubePlaylist> {
+        val context = Context(
+            Context.WEB_REMIX_DEFAULT.client.copy(
+                hl = localization.languageCode,
+                gl = localization.regionCode
+            )
+        )
+        val browseBody = BrowseBody.builder( context ).browseId( playlistId ).build()
+
+        return runCatching {
+            val response = ytmBrowse( browseBody, Constants.JSON_HEADERS )
+            val browseResponse = JSON.decodeFromString<BrowseResponseImpl>( response.responseBody )
+
+            InnertubePlaylistImpl.from(
+                browseResponse.responseContext.visitorData!!,
+                browseResponse.contents.twoColumnBrowseResultsRenderer!!
+            )
+        }
+    }
+
+    /**
+     * Request for more songs in a playlist.
+     *
+     *
+     * `visitorData` and `continuation` are parameters
+     * extracted from response when you browse for playlist.
+     *
+     * @param visitorData tracking parameter extracted from browse playlist response
+     * @param continuation unique string of playlist to get next songs
+     * @param params additional parameters (optional)
+     */
+    fun playlistContinued(
+        visitorData: String,
+        continuation: String,
+        localization: Localization,
+        params: String?
+    ): Result<ContinuedPlaylist> {
+
+        val context = Context(
+            Context.WEB_REMIX_DEFAULT.client.copy(
+                hl = localization.languageCode,
+                gl = localization.regionCode,
+                visitorData = visitorData
+            )
+        )
+        val browseBody = BrowseBody.builder( context ).continuation( continuation ).params( params ).build()
+
+        return runCatching {
+            val response = ytmBrowse( browseBody, Constants.JSON_HEADERS )
+            val browseResponse = JSON.decodeFromString<BrowseResponseImpl>( response.responseBody )
+
+            ContinuedPlaylistImpl.from(
+                browseResponse.onResponseReceivedActions
+                              .first()
+                              .appendContinuationItemsAction
+                              .continuationItems
+            )
+        }
+    }
+
+    interface Provider {
+
+        @Blocking
+        @Throws(IOException::class)
+        fun execute(request: Request): Response
+    }
+}
---
+++ src/test/kotlin/me/knighthat/innertube/InnertubeTest.kt
@@ -0,0 +1,115 @@
+package me.knighthat.innertube
+
+import me.knighthat.innertube.request.Request
+import me.knighthat.innertube.request.body.Context
+import me.knighthat.innertube.request.body.NextBody
+import me.knighthat.innertube.request.body.SearchSuggestionsBody
+import me.knighthat.innertube.response.Response
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.logging.HttpLoggingInterceptor
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.extension.MediaType
+import java.io.BufferedReader
+import java.io.IOException
+import java.util.zip.GZIPInputStream
+
+
+class InnertubeTest {
+
+    companion object {
+
+        @BeforeAll
+        @JvmStatic
+        fun setup() { Innertube.client = InnertubeProvider() }
+    }
+
+    @Test
+    fun testSendRequestToYouTube() {
+        // Fabricating & sending request within [Assertions.assertDoesNotThrow]
+        // to avoid try/catch
+        val response = assertDoesNotThrow {
+            val body: NextBody = NextBody.builder( Context.WEB_DEFAULT )
+                                         .videoId( "lYBUbBu4W08" )
+                                         .build()
+            Innertube.sendRequest(
+                Request.POST, Constants.YOUTUBE_URL, Endpoints.NEXT, body, Constants.JSON_HEADERS
+            )
+        }
+
+        assertEquals( 200, response.responseCode )
+        assertFalse( response.responseBody.isBlank() )
+    }
+
+    @Test
+    fun testSendRequestToYouTubeMusic() {
+        val response = assertDoesNotThrow {
+            val body: SearchSuggestionsBody = SearchSuggestionsBody.builder( Context.WEB_REMIX_DEFAULT )
+                                                                   .input( "million dollar baby" )
+                                                                   .build()
+            Innertube.sendRequest(
+                Request.POST, Constants.YOUTUBE_MUSIC_URL, Endpoints.NEXT, body, Constants.JSON_HEADERS
+            )
+        }
+
+        assertEquals( 200, response.responseCode )
+        assertFalse( response.responseBody.isBlank() )
+    }
+
+    class InnertubeProvider: Innertube.Provider {
+
+        private val HEADER_INTERCEPTOR = HttpLoggingInterceptor().setLevel( HttpLoggingInterceptor.Level.HEADERS )
+        private val BODY_INTERCEPTOR = HttpLoggingInterceptor().setLevel( HttpLoggingInterceptor.Level.BODY )
+
+        val CLIENT = OkHttpClient.Builder().addInterceptor( HEADER_INTERCEPTOR ).addInterceptor( BODY_INTERCEPTOR ).build()
+
+        @Throws(IOException::class)
+        override fun execute( request: Request ): Response {
+            val builder = okhttp3.Request.Builder()
+
+            // Add headers
+            request.headers
+                   .mapValues { it.value.joinToString() }
+                   .forEach( builder::addHeader )
+
+            // Destination (with prettyPrint turned off)
+            builder.url(
+                url = request.url
+                             .toHttpUrl()
+                             .newBuilder()
+                             .addQueryParameter( "prettyPrint", "false" )
+                             .build()
+            )
+
+            // Add method and payload (body)
+            when ( request.httpMethod ) {
+                Request.GET     -> builder.get()
+                Request.POST    -> {
+                    builder.post(
+                        body = JSON.encodeToString( request.dataToSend )
+                                   .toRequestBody( MediaType.APPLICATION_JSON.toString().toMediaType() )
+                    )
+                }
+
+                else            -> throw UnsupportedOperationException("Unknown method ${request.httpMethod}")
+            }
+
+
+            val result = CLIENT.newCall( builder.build() ).execute().use { response ->
+                Response(
+                    response.code,
+                    response.message,
+                    response.headers.toMultimap(),
+                    GZIPInputStream( response.body!!.byteStream() ).bufferedReader().use(BufferedReader::readText )
+                )
+            }
+            return result
+        }
+    }
+}
