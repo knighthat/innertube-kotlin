698fc6d00e79dc3d295d3ee3410f601299164411
knighthat <git@knighthat.me>
2025-09-26 13:54:40 -0500

    split MetadataBadge & MusicInlineBadge

--- src/main/kotlin/me/knighthat/innertube/response/Badge.kt
+++ src/main/kotlin/me/knighthat/innertube/response/Badge.kt
@@ -7,14 +7,23 @@ interface Badge {
         const val EXPLICIT: String = "MUSIC_EXPLICIT_BADGE"
     }
 
-    val musicInlineBadgeRenderer: Renderer?
-    val metadataBadgeRenderer: Renderer?
+    val musicInlineBadgeRenderer: MusicInlineBadge?
+    val metadataBadgeRenderer: MetadataBadge?
 
     interface Renderer {
 
         val style: String?
         val tooltip: String?
-        val accessibilityData: Accessibility?
         val icon: Icon?
+    }
+
+    interface MusicInlineBadge: Renderer {
+
+        val accessibilityData: Accessibility?
+    }
+
+    interface MetadataBadge: Renderer {
+
+        val accessibilityData: Accessibility.Data?
     }
 }
--- src/main/kotlin/me/knighthat/internal/response/BadgeImpl.kt
+++ src/main/kotlin/me/knighthat/internal/response/BadgeImpl.kt
@@ -2,25 +2,26 @@ package me.knighthat.internal.response
 
 import kotlinx.serialization.Serializable
 import me.knighthat.innertube.response.Badge
-import me.knighthat.innertube.response.Icon
 
 @Serializable
 internal data class BadgeImpl(
-    override val musicInlineBadgeRenderer: RendererImpl?,
-    override val metadataBadgeRenderer: RendererImpl?
+    override val musicInlineBadgeRenderer: MusicInlineBadgeImpl?,
+    override val metadataBadgeRenderer: MetadataBadgeImpl?
 ): Badge {
 
     @Serializable
-    internal data class RendererImpl(
+    data class MusicInlineBadgeImpl(
+        override val accessibilityData: AccessibilityImpl?,
         override val style: String?,
         override val tooltip: String?,
-        override val accessibilityData: AccessibilityImpl?,
-        override val icon: IconImpl
-    ): Badge.Renderer {
+        override val icon: IconImpl?
+    ): Badge.MusicInlineBadge
 
-        @Serializable
-        internal data class IconImpl(
-            override val iconType: String
-        ): Icon
-    }
+    @Serializable
+    data class MetadataBadgeImpl(
+        override val accessibilityData: AccessibilityImpl.DataImpl?,
+        override val style: String?,
+        override val tooltip: String?,
+        override val icon: IconImpl?
+    ): Badge.MetadataBadge
 }
